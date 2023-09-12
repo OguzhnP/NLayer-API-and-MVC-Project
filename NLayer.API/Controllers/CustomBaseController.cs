c8c6d1795b219bb3086e237a1a7a76b3a9fbc5e8
Oguzhan Portakal <101134423+OguzhnP@users.noreply.github.com>
2023-09-12 20:29:17 +0300

    Added  Products Controller

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+  
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> responseDto)
+        {
+            if (responseDto.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = responseDto.StatusCode,
+                };
+            return new ObjectResult(responseDto)
+            {
+                StatusCode = responseDto.StatusCode
+            };
+        }
+    }
+}
