8f1bcc4b5670eaa2a29a27aa6b0bb9180bff2381
Oguzhan Portakal <101134423+OguzhnP@users.noreply.github.com>
2023-09-12 15:52:00 +0300

    Created Interfaces

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        //productRepo.where(x=>x.id< 6).orderBy().toList();
+        //veri tabanına gitmeden önce şartı sorgulyıp daha sonra sıralayıp getirmesi için böyle bir where sorgusu oluşturulur
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
