64cce73483f81f72e0bbfdaac4c9956a1bf023d7
Oguzhan Portakal <101134423+OguzhnP@users.noreply.github.com>
2023-09-12 16:48:29 +0300

    Added seed data

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -19,6 +19,22 @@ namespace NLayer.Repository
 
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id=1,
+                Color="Kırmızı",
+                Height=100,
+                Width=200,
+                ProductId=1
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 300,
+                Width = 400,
+                ProductId = 2,
+            });
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, CategoryId = 1, Price = 100, Stock = 20, Name = "Kalem1", CreatedDate = DateTime.Now },
+                new Product { Id = 2, CategoryId = 1, Price = 222, Stock = 21, Name = "Kalem2", CreatedDate = DateTime.Now },
+                new Product { Id = 3, CategoryId = 1, Price = 333, Stock = 23, Name = "Kalem3", CreatedDate = DateTime.Now },
+                new Product { Id = 5, CategoryId = 2, Price = 444, Stock = 25, Name = "Kitap1", CreatedDate = DateTime.Now },
+                new Product { Id = 6, CategoryId = 3, Price = 555, Stock = 24, Name = "Defter1", CreatedDate = DateTime.Now }
+                );
+        }
+    }
+}
